d9e77a6364c89b3aa63ec7a743cd8a1458a32edc
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-08 16:54:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -379,8 +379,8 @@ end
 def long_name_steals_a_ton?
   name = player_with_longest_name
   steals = most_steals
-  puts name
-  puts steals
+  #puts name
+  #puts steals
   val = name == steals ? true : false
   #puts val
   return val
