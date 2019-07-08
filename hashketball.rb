144b810e8d16060b4e867dd8818f08dc63deb691
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-08 16:55:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -360,9 +360,9 @@ def most_steals
   playersName = ""
   
   all_stats.each do |players|
-    puts players
+    #puts players
     players.each do |name, val|
-      #puts val
+      puts val
       #puts val[:steals]
       if val[:steals] > maxSteals
         playersName = name
